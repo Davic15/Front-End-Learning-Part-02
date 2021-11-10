cb70b37321ed7134248d01c6f06ef90723bd294c
Franklin David Macias Avellan <franklin.macias864@gmail.com>
2021-11-10 16:16:16 +0100

    Module 3: Making websites interative (JavaScript). Project - 1 Counting People

--- Module-03/Project-01-Counting-People/js/index.js
+++ Module-03/Project-01-Counting-People/js/index.js
@@ -1,10 +1,14 @@
 let count = 0;
 let countEl = document.getElementById('count-el');
+let saveEl = document.getElementById('save-el');
 
 function increment() {
-    countEl.innerHTML = count += 1;
+    countEl.textContent = count += 1;
 }
 
 function save() {
-    console.log(count);
+    let counter = count + " - ";
+    saveEl.textContent += counter;
+    count = 0;
+    countEl.textContent = 0;
 }
