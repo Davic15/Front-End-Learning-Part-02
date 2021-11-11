e7b595ff50487d87367a15e79c29f4404318d72a
Franklin David Macias Avellan <franklin.macias864@gmail.com>
2021-11-11 20:04:11 +0100

    Module 3: Making websites interative (JavaScript). Project - 2 Blackjack

--- Module-03/Project-02-Blackjack/js/index.js
+++ Module-03/Project-02-Blackjack/js/index.js
@@ -1,22 +1,39 @@
-let firstCard = 5;
-let secondCard = 11;
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackjack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let mesageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 //let sumEl = document.querySelector("#sum-el");
 
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 12) + 1;
+    if (randomNumber > 10) {
+        return 10;
+    } else if (randomNumber ===1){
+        return 11;
+    } else {
+        return randomNumber;
+    }
+}
+
 function startGame() {
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
     renderGame();
 }
 
 function renderGame() {
     sumEl.textContent = "Sum: " + sum;
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+    cardsEl.textContent = "Cards: ";
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " ";
+    }
     if (sum < 21) {
         message = "Do you want to draw a new card?";
     } else if (sum === 21) {
@@ -30,7 +47,8 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = 5;
+    let card = getRandomCard();
     sum += card;
+    cards.push(card);
     renderGame();
 }
