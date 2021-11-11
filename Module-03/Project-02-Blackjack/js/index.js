29fb39e218ccbb46e41e7d487d6c8942b6036cbc
Franklin David Macias Avellan <franklin.macias864@gmail.com>
2021-11-11 22:45:35 +0100

    Module 3: Making websites interative (JavaScript). Project - 2 Blackjack

--- Module-03/Project-02-Blackjack/js/index.js
+++ Module-03/Project-02-Blackjack/js/index.js
@@ -13,7 +13,6 @@ let player = {
 
 let playerEl = document.getElementById("player-el");
 playerEl.textContent = player.name + ": $" + player.chips;
-//let sumEl = document.querySelector("#sum-el");
 
 function getRandomCard() {
     let randomNumber = Math.floor(Math.random() * 12) + 1;
