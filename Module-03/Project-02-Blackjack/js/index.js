d6f9edf4d6159f2acbd32e3f65a4d7baa0bc8945
Franklin David Macias Avellan <franklin.macias864@gmail.com>
2021-11-11 18:30:58 +0100

    Module 3: Making websites interative (JavaScript). Project - 2 Blackjack

--- Module-03/Project-02-Blackjack/js/index.js
+++ Module-03/Project-02-Blackjack/js/index.js
@@ -1,11 +1,36 @@
-let firstCard = 10;
+let firstCard = 5;
 let secondCard = 11;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
+let hasBlackjack = false;
+let isAlive = true;
+let message = "";
+let mesageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+//let sumEl = document.querySelector("#sum-el");
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card?");
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack!");
-} else {
-    console.log("You're out of the game! 😭")
+function startGame() {
+    renderGame();
+}
+
+function renderGame() {
+    sumEl.textContent = "Sum: " + sum;
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+    if (sum < 21) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackjack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+    mesageEl.textContent = message;
+}
+
+function newCard() {
+    let card = 5;
+    sum += card;
+    renderGame();
 }
