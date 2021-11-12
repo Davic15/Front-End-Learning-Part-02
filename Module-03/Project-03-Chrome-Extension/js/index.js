73d9cfef0693899fa0bfd106b7a0770db8016142
Franklin David Macias Avellan <franklin.macias864@gmail.com>
2021-11-12 16:42:46 +0100

    Module 3: Making websites interative (JavaScript). Project - 3 Chrome Extension

--- Module-03/Project-03-Chrome-Extension/js/index.js
+++ Module-03/Project-03-Chrome-Extension/js/index.js
@@ -2,25 +2,37 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-localStorage.setItem("myLeads", "www.google.com.ec");
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
+}
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    renderLeads();
-})
-
-function renderLeads() {
+function render(leads) {
     let listItems = "";
-    for (let i = 0; i < myLeads.length; i++){
+    for (let i = 0; i < leads.length; i++){
         listItems += `
             <li>
-                <a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a>
+                <a href="${leads[i]}" target="_blank">${leads[i]}</a>
             </li>
         `;
     }
     ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+});
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+});
+
+
