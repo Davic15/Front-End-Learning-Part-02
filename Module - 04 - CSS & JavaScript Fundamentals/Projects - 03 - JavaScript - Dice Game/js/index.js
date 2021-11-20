c42dee7769c96b7442bae08e843c6057736c00d0
Franklin David Macias Avellan <franklin.macias864@gmail.com>
2021-11-20 11:54:09 +0100

    Module 4: CSS & JavaScript fundamentals. JavaScript - Dice game

--- Module - 04 - CSS & JavaScript Fundamentals/Projects - 03 - JavaScript - Dice Game/js/index.js
+++ Module - 04 - CSS & JavaScript Fundamentals/Projects - 03 - JavaScript - Dice Game/js/index.js
@@ -12,7 +12,25 @@ const rollBtn = document.getElementById("rollBtn");
 const resetBtn = document.getElementById("resetBtn");
 const message = document.getElementById("message");
 
+//player1Dice.classList.remove("active");
+
 rollBtn.addEventListener("click", function() {
     const randomNumber = Math.floor(Math.random() * 6) + 1;
-})
+
+
+    if (player1Turn){
+        player1Dice.textContent = randomNumber;
+        player1Dice.classList.remove("active");
+        player2Dice.classList.add("active");
+        message.textContent = "Player 1 Turn";
+        player1Scoreboard.textContent = randomNumber;
+    } else {
+        player2Dice.textContent = randomNumber;
+        player2Dice.classList.remove("active");
+        player1Dice.classList.add("active");
+        message.textContent = "Player 2 Turn";
+        player2Scoreboard.textContent = randomNumber;
+    }
+    player1Turn = !player1Turn;
+});
 
