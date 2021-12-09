e18f2daf3f62c57fbad8bf03eed879ffa856552b
DESKTOP-A953543\Frank <franklin.macias864@gmail.com>
2021-12-09 22:42:00 +0100

    Added Module 5 Responsive Design: Example 07 - BBQ Splash Page

---
+++ Module - 05 - Responsive Design/Example - 07 - BBQ Splash Page/js/index.js
@@ -0,0 +1,16 @@
+let emailCollectorForm = document.getElementById("Email-Collector");
+emailCollectorForm.addEventListener("submit", event => {
+    event.preventDefault();
+    let ourFormData = new FormData(event.target);
+    let userFirstName = ourFormData.get("firstName");
+    let updateHtmlContent = `
+        <h2>Congratulation, ${userFirstName} </h2>
+        <p>
+            You're on your way to becoming a BBQ Master!
+        </p>
+        
+        <p class="fine-print">We'll never share your information without your permission</p>
+    `;
+    let ourMainContent = document.getElementById("Main-Content");
+    ourMainContent.innerHTML = updateHtmlContent;
+});
