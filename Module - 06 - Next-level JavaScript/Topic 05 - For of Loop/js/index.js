d7ea3ffbeee38c67a04af85452eeb45ee72d0dd6
DESKTOP-A953543\Frank <franklin.macias864@gmail.com>
2021-12-10 17:50:46 +0100

    Added Module 6 Next-level JavaScript: For of Loop

---
+++ Module - 06 - Next-level JavaScript/Topic 05 - For of Loop/js/index.js
@@ -0,0 +1,13 @@
+let incomes = [62000, 67000, 75000];
+let total = 0;
+
+for (const income of incomes) {
+    total += income;
+}
+console.log(total);
+
+let fullName = "David Macias";
+for (const fullNames of fullName) {
+    console.log(fullNames);
+}
+
