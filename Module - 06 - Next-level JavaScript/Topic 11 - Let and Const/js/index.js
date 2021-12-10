61e8229bbb929359a71c64dbb26e63f7f1c6f790
DESKTOP-A953543\Frank <franklin.macias864@gmail.com>
2021-12-10 19:09:39 +0100

    Added Module 6 Next-level JavaScript: Let & Const

---
+++ Module - 06 - Next-level JavaScript/Topic 11 - Let and Const/js/index.js
@@ -0,0 +1,7 @@
+// let block definitions
+// with const we can add values on any array or objects, but we cannot assign a new value on them
+
+const example = {};
+example.name = "David"
+example.surname = "Macias"
+console.log(example)
