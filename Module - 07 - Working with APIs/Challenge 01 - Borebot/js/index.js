afc8f4488fd87dec9009eba7cf8bbd5d57cb24ab
DESKTOP-A953543\Frank <franklin.macias864@gmail.com>
2021-12-12 00:47:14 +0100

    Added Module 7 - Working with APIs: BoredBot API

--- Module - 07 - Working with APIs/Challenge 01 - Borebot/js/index.js
+++ Module - 07 - Working with APIs/Challenge 01 - Borebot/js/index.js
@@ -1,14 +1,15 @@
 const btn = document.querySelector(".btn");
 const header4 = document.querySelector(".todo");
+const header1 = document.getElementById("title");
 
 
 btn.addEventListener("click", function() {
     fetch("https://apis.scrimba.com/bored/api/activity")
         .then(response => response.json())
         .then(data => {
-            
-        console.log(data.activity)
         header4.textContent = data.activity;
+        header1.textContent = "🦾 HappyBot 🦿";
+        document.body.classList.add("fun")
     });
 });
 
