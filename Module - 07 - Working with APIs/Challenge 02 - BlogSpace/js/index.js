1c48d736e5b18e5e2105d3d72dbcb822774c16a9
DESKTOP-A953543\Frank <franklin.macias864@gmail.com>
2021-12-13 00:59:09 +0100

    Added Module 7 - Working with APIs: BlogSpace

--- Module - 07 - Working with APIs/Challenge 02 - BlogSpace/js/index.js
+++ Module - 07 - Working with APIs/Challenge 02 - BlogSpace/js/index.js
@@ -1,15 +1,39 @@
 const divEl = document.querySelector(".container");
+const formEl = document.getElementById("new-post");
+
 let htmlFragment = "";
 fetch("https://apis.scrimba.com/jsonplaceholder/posts", {method: "GET"})
     .then(res => res.json())
     .then(data => {
-        const postArray = data.slice(0, 5)
+        const postArray = data.slice(0, 5);
         for(const element of postArray){
             htmlFragment += `
                 <h3>${element.title}</h3>
                 <p>${element.body}</p>
                 <hr />
-            `
+            `;
         }
-        divEl.innerHTML = htmlFragment
+        divEl.innerHTML = htmlFragment;
     });
+
+    formEl.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const postTitle = document.getElementById("post-title").value;
+        const bodyTitle = document.getElementById("post-body").value;
+        const data = {
+            title: postTitle,
+            body: bodyTitle
+        }
+        
+        const options = {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+
+        fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(data => console.log(data))
+    })
