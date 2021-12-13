bb81a3a9ab0d84f460624c7de09cd581205edb95
DESKTOP-A953543\Frank <franklin.macias864@gmail.com>
2021-12-13 22:37:52 +0100

    Added Module 7 - Working with APIs: BlogSpace

--- Module - 07 - Working with APIs/Challenge 02 - BlogSpace/js/index.js
+++ Module - 07 - Working with APIs/Challenge 02 - BlogSpace/js/index.js
@@ -1,48 +1,49 @@
 const divEl = document.querySelector(".container");
 const formEl = document.getElementById("new-post");
-let htmlFragment = "";
+const titleInput = document.getElementById("post-title");
+const bodyInput = document.getElementById("post-body");
+let postsArray = [];
+
+function renderPost() {
+    let htmlFragment = "";
+    for(const element of postsArray){
+        htmlFragment += `
+            <h3>${element.title}</h3>
+            <p>${element.body}</p>
+            <hr />
+        `;
+    }
+    divEl.innerHTML = htmlFragment;
+}
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts", {method: "GET"})
     .then(res => res.json())
     .then(data => {
-        const postArray = data.slice(0, 5);
-        for(const element of postArray){
-            htmlFragment += `
-                <h3>${element.title}</h3>
-                <p>${element.body}</p>
-                <hr />
-            `;
-        }
-        divEl.innerHTML = htmlFragment;
+        postsArray = data.slice(0, 5);
+        renderPost();
     });
 
-    formEl.addEventListener("submit", function(e) {
-        e.preventDefault();
-        const postTitle = document.getElementById("post-title").value;
-        const bodyTitle = document.getElementById("post-body").value;
-        const data = {
-            title: postTitle,
-            body: bodyTitle
+formEl.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const postTitle = titleInput.value;
+    const bodyTitle = bodyInput.value;
+    const data = {
+        title: postTitle,
+        body: bodyTitle
+    }
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
         }
-        
-        const options = {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }
+    }
 
-        fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
-        .then(res => res.json())
-        .then(post => {
-            htmlFragment = `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-                ${divEl.innerHTML}
-            `;
-            divEl.innerHTML = htmlFragment;
-
-        })
-    })
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+    .then(res => res.json())
+    .then(post => {
+        postsArray.unshift(post);           
+        renderPost();
+        formEl.reset();
+    });
+});
