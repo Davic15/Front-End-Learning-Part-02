41349c54a6a9d4865d9b3c8a0098bdf17f1f0cbe
DESKTOP-A953543\Frank <franklin.macias864@gmail.com>
2021-12-14 23:46:18 +0100

    Added Module 7 - Working with APIs: War Project

--- Module - 07 - Working with APIs/Challenge 03 - War Project/js/index.js
+++ Module - 07 - Working with APIs/Challenge 03 - War Project/js/index.js
@@ -1,20 +1,25 @@
 let deckId = "";
+let computerScore = 0;
+let myScore = 0;
 const cardsContainer = document.getElementById("cards");
 const newDeckBtn = document.getElementById("new-deck");
 const drawCardBtn = document.getElementById("draw-cards");
 const header = document.getElementById("header");
-const remainingText = document.getElementById("remaining")
+const remainingText = document.getElementById("remaining");
+const computerScoreEl = document.getElementById("computer-score");
+const myScoreEl = document.getElementById("my-score");
 
 function handleClick() {
     fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            console.log(data);
             remainingText.innerText = `Reamining Cards: ${data.remaining}`;
             deckId = data.deck_id;
         });      
 }
-newDeckBtn.addEventListener("click", handleClick)
+newDeckBtn.addEventListener("click", handleClick);
+
 drawCardBtn.addEventListener("click", () => {
     fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
     .then(res => res.json())
@@ -24,6 +29,16 @@ drawCardBtn.addEventListener("click", () => {
         cardsContainer.children[1].innerHTML = `<img src=${data.cards[1].image} class="card" />`
         const winnerText = determineCardWinner(data.cards[0].value, data.cards[1].value);
         header.innerText = winnerText;
+        if (data.remaining === 0) {
+            drawCardBtn.disabled = true;
+            if (computerScore > myScore) {
+                header.textContent = "The computer won the game!"
+            } else if (myScore > computerScore) {
+                header.textContent = "You won the game!"
+            } else {
+                header.textContent = "It's a tie game!"
+            } 
+        }
     });
 });
 
@@ -37,9 +52,13 @@ function determineCardWinner(card1, card2) {
     if (card1ValueIndex === card2ValueIndex) {
         message = "War!";
     } else if (card1ValueIndex > card2ValueIndex) {
-        message = "Card 1 wins!";
+        message = "Computer wins!";
+        computerScore += 1;
+        computerScoreEl.innerText = `Computer Score: ${computerScore}`;
     } else {
-        message = "Card 2 wins!";
+        message = "You win!";
+        myScore += 1;
+        myScoreEl.innerText = `My score: ${myScore}`
     }
     return message;
 }
