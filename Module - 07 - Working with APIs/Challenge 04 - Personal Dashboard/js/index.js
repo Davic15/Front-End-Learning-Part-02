cd4466f19ca8353ced33f6a5e96ae2a124d5a38a
DESKTOP-A953543\Frank <franklin.macias864@gmail.com>
2021-12-15 12:32:01 +0100

    Added Module 7 - Working with APIs: Personal Dashboard

--- Module - 07 - Working with APIs/Challenge 04 - Personal Dashboard/js/index.js
+++ Module - 07 - Working with APIs/Challenge 04 - Personal Dashboard/js/index.js
@@ -2,6 +2,12 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
 .then(res => res.json())
 .then(data => {
-    console.log(data.urls.full);
+    console.log(data);
+    console.log(`Author: ${data.user.first_name} ${data.user.last_name}`)
     document.body.style.backgroundImage = `url(${data.urls.full})`
+    document.getElementById("author").innerText = `By: ${data.user.first_name} ${data.user.last_name}`
+    
+})
+.catch(err => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
 })
