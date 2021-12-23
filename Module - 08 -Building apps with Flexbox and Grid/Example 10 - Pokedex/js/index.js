3c8db4e756e5a53130f708d58b25f71528f5525c
DESKTOP-A953543\Frank <franklin.macias864@gmail.com>
2021-12-23 20:57:48 +0100

    Module 08: Building apps with Flexbox and Grid - Example 09 - Simple Pokedex

---
+++ Module - 08 -Building apps with Flexbox and Grid/Example 10 - Pokedex/js/index.js
@@ -0,0 +1,31 @@
+async function getAllPokemon() {
+    let response = await fetch("./json/pokemon.json");
+    let data = await response.json();
+    console.log(data);
+    return data;
+}
+function getPokemonHtml(aPokemon) {
+    return `
+        <div class="a-pokemon">
+            <div class="a-pokemon-id">${aPokemon.id}</div>
+            
+            <div class="a-pokemon-name">${aPokemon.name.english}</div>
+            <div class="a-pokemon-type">${aPokemon.type.join(' / ')}</div>
+            
+            <div class="a-pokemon-stat">HP: ${aPokemon.base.HP}</div>
+            <div class="a-pokemon-stat">Attack: ${aPokemon.base.Attack}</div>
+            <div class="a-pokemon-stat">Defense: ${aPokemon.base.Defense}</div>
+            <div class="a-pokemon-stat">Speed: ${aPokemon.base.Speed}</div>
+        </div>
+    `;
+}
+
+getAllPokemon().then(allPokemon => {
+    console.log(allPokemon[0]);
+    let samplePokemon = allPokemon[0]
+    document.body.innerHTML = `
+        <div class="my-pokedex">
+            ${allPokemon.map(aPokemon => getPokemonHtml(aPokemon)).join('')}
+        </div>
+    `;
+});
