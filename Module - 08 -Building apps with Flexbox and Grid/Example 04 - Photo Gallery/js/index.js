2d340879784fa9bcf36ec67e7547f35a15c721c5
DESKTOP-A953543\Frank <franklin.macias864@gmail.com>
2021-12-23 16:34:41 +0100

    Module 08: Building apps with Flexbox and Grid - Example 04 - Photo Gallery

--- Module - 08 -Building apps with Flexbox and Grid/Example 04 - Photo Gallery/js/index.js
+++ Module - 08 -Building apps with Flexbox and Grid/Example 04 - Photo Gallery/js/index.js
@@ -1,13 +1,46 @@
+/* 
+    2. 
+       Write a function to create the my-photos Component
+       
+       Create a Column Flexbox container around my-photos
+       
+       Create a new img and place it, centered, above my-photos
+        -This should be hidden initially
+        -Also give it the my-photo class
+    
+       Create a "click" event for each photo in my-photos
+        -When clicked, load the clicked photo into the new img tag
+*/
+
 async function getPhotos() {
     let response = await fetch("./json/photos.json");
     let photos = await response.json();
     return photos;
 }
 
-
-getPhotos().then(photos => {
+function getPhotosHtml(photos) {
     let myPhotosHtml = photos.map(photo => {
-        return `<img class="my-photo" src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}"/>`;
+        return `<img class="my-photo" src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}"/>`
     }).join('');
-    document.body.innerHTML = `<div class="my-photos">${myPhotosHtml}</div>`;
+    console.log(myPhotosHtml);
+    
+    return `<div class="my-photos">${myPhotosHtml}</div>`;   
+}
+
+getPhotos().then(photos => {    
+    document.body.innerHTML = `
+        <div class="my-gallery">
+            <img style="display: none;" class="my-photo" id="my-selected-photo" src="https://picsum.photos/id/1/200/200" />
+            ${getPhotosHtml(photos)}
+        </div>`;   
+    
+    let myPhotoImgs = Array.from(document.getElementsByClassName("my-photo"));
+        myPhotoImgs.forEach(photoImg => {
+        photoImg.addEventListener("click", event => {
+            let selectedPhotoSrc = `${photoImg.src.substr(0, photoImg.src.length - 7)}200/200`;
+            let selectedPhoto = document.getElementById("my-selected-photo");
+            selectedPhoto.src = selectedPhotoSrc;
+            selectedPhoto.style.display = "inline";
+        })
+    })
 })
