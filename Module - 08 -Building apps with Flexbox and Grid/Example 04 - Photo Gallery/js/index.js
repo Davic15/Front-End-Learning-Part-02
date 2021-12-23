725edcc5615ec8f9e8c7eb829c39f279c76f75db
DESKTOP-A953543\Frank <franklin.macias864@gmail.com>
2021-12-23 16:40:09 +0100

    Module 08: Building apps with Flexbox and Grid - Example 04 - Photo Gallery

--- Module - 08 -Building apps with Flexbox and Grid/Example 04 - Photo Gallery/js/index.js
+++ Module - 08 -Building apps with Flexbox and Grid/Example 04 - Photo Gallery/js/index.js
@@ -1,46 +1,31 @@
-/* 
-    2. 
-       Write a function to create the my-photos Component
-       
-       Create a Column Flexbox container around my-photos
-       
-       Create a new img and place it, centered, above my-photos
-        -This should be hidden initially
-        -Also give it the my-photo class
-    
-       Create a "click" event for each photo in my-photos
-        -When clicked, load the clicked photo into the new img tag
-*/
-
 async function getPhotos() {
     let response = await fetch("./json/photos.json");
     let photos = await response.json();
     return photos;
 }
 
-function getPhotosHtml(photos) {
+function getMyPhotosHtml(photos) {
     let myPhotosHtml = photos.map(photo => {
-        return `<img class="my-photo" src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}"/>`
-    }).join('');
-    console.log(myPhotosHtml);
-    
-    return `<div class="my-photos">${myPhotosHtml}</div>`;   
+        return `<img class="my-photo" src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}"/>`;
+    }).join('')
+    return `<div class="my-photos">${myPhotosHtml}</div>`;
 }
 
-getPhotos().then(photos => {    
-    document.body.innerHTML = `
-        <div class="my-gallery">
-            <img style="display: none;" class="my-photo" id="my-selected-photo" src="https://picsum.photos/id/1/200/200" />
-            ${getPhotosHtml(photos)}
-        </div>`;   
+getPhotos().then(photos => {
+    let myPhotosHtml = getMyPhotosHtml(photos);
+    document.body.innerHTML = `<div class="my-gallery">
+        <img id="my-selected-photo" class="my-photo"
+        src="https://picsum.photos/id/${photos[2].id}/300/300" />
+        ${myPhotosHtml}
+    </div>`;
     
     let myPhotoImgs = Array.from(document.getElementsByClassName("my-photo"));
-        myPhotoImgs.forEach(photoImg => {
-        photoImg.addEventListener("click", event => {
-            let selectedPhotoSrc = `${photoImg.src.substr(0, photoImg.src.length - 7)}200/200`;
+    myPhotoImgs.forEach(photoImg => {
+        photoImg.addEventListener("click", e => {
+            let selectedPhotoSrc = photoImg.src.substr(0, photoImg.src.length - 7) + `300/300`;
             let selectedPhoto = document.getElementById("my-selected-photo");
             selectedPhoto.src = selectedPhotoSrc;
             selectedPhoto.style.display = "inline";
         })
-    })
+    })    
 })
