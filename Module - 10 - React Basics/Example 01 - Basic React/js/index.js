89812dcc205eb478b7afbce45ec32d9bf3916885
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-01-15 15:17:10 +0100

    Module 10 - React Basics: Example 01

--- Module - 10 - React Basics/Example 01 - Basic React/js/index.js
+++ Module - 10 - React Basics/Example 01 - Basic React/js/index.js
@@ -1,2 +1,13 @@
-ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"))
+const navbar = (
+    <nav>
+        <h1>Bob's Bistro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
 ReactDOM.render(<p>Hola mundo</p>, document.getElementById("root-two"))
