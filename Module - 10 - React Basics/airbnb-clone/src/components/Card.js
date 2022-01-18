367741ced7cfced0559efe1ed2c5e7ef52a5e30e
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-01-18 14:14:55 +0100

    Module 10 - React Basics: airbnb clone

--- Module - 10 - React Basics/airbnb-clone/src/components/Card.js
+++ Module - 10 - React Basics/airbnb-clone/src/components/Card.js
@@ -2,14 +2,20 @@ import React from "react";
 import starUser from '../images/star.png';
 
 export default function Card(props) {
+    let badgeText;
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT";
+    } else if (props.location === "Online"){
+        badgeText = "ONLINE";
+    }
     return (
         <div className="card">
-            {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
-            <img src={`../images/${props.img}`} className="card--image" alt="user experiences"/>
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={`../images/${props.coverImg}`} className="card--image" alt="user experiences"/>
             <div className="card--stats">
                 <img src={starUser} className="card--star" alt="rating star"/>
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
                 <span className="gray">{props.location}</span>
             </div>
             <p className="card--title">{props.title}</p>
