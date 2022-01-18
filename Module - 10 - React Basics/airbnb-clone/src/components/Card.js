59da73f9972c7f904b01f0b4f8a063c51c525618
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-01-18 12:17:00 +0100

    Module 10 - React Basics: airbnb clone

--- Module - 10 - React Basics/airbnb-clone/src/components/Card.js
+++ Module - 10 - React Basics/airbnb-clone/src/components/Card.js
@@ -1,19 +1,19 @@
 import React from "react";
-import imageUser from '../images/katie-zaferes.png';
 import starUser from '../images/star.png';
 
-export default function Card() {
+export default function Card(props) {
+    console.log(props.img)
     return (
         <div className="card">
-            <img src={imageUser} alt="users" className="card--image"/>
+            <img src={`../images/${props.img}`} className="card--image" alt="user experiences"/>
             <div className="card--stats">
-                <img src={starUser} alt="rate star" className="card--star"/>
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
-            </div> 
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+                <img src={starUser} className="card--star" alt="rating star"/>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
+            </div>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
-    );
+    )
 }
